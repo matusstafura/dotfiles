bf6a1f7bfbcd203ad6dfa88cde799e5030b0364b
Matus <kobe21@gmail.com>
2021-11-13 12:47:20 +0100

    tmux open window in same dir; update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+git clone git@github.com:matusstafura/dotfiles.git ~/.dotfiles
+
 # install packages
 sudo apt install tmux fzf -y
-
-mv ~/dotfiles ~/.dotfiles
+cd ~ && curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage && chmod u+x nvim.appimage && sudo mv ./nvim.appimage /usr/bin/nvim
 
 # install zsh and theme p10k
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -15,6 +16,7 @@ ln -sf ~/.dotfiles/.p10k.zsh ~
 ln -sf ~/.dotfiles/.tmux.conf ~
 ln -sf ~/.dotfiles/.vimrc ~
 ln -sf ~/.dotfiles/.zshrc ~
+mkdir -P ~/.config/nvim && ln -sf ~/.dotfiles/.init.vim ~/.config/nvim/init.vim
 
 # reload config files
 source ~/.zshrc
