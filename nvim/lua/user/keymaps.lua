a78e03d8eeae7a225a138b78145033d21bcbdc40
Matus Stafura <kobe21@gmail.com>
2023-02-04 15:27:36 +0100

    keymap insert save

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,9 +1,14 @@
+-- remap backslash to leader key
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
 
+-- remap jj to <Esc>
 vim.keymap.set('i', 'jj', '<Esc>')
+
+-- save and quit
 vim.keymap.set('n', '<Leader>q', '<Esc>:q!<CR>')
 vim.keymap.set('n', '<Leader>z', '<Esc>:wq<CR>')
 vim.keymap.set('n', '<Leader>s', '<Esc>:w!<CR>')
+vim.keymap.set('i', '<Leader>s', '<Esc>:w!<CR>')
 
 
