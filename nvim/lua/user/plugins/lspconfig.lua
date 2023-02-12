f74f6ec0cb4714a3c9d49abe790c6c8e0db88be8
Matus Stafura <kobe21@gmail.com>
2023-02-12 15:14:38 +0100

    lsp keymap

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -1,14 +1,23 @@
 require('mason').setup()
 require('mason-lspconfig').setup({automatic_installation = true})
 
+vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { buffer = bufnr })
+vim.keymap.set('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<CR>', { buffer = bufnr })
+vim.keymap.set('n', '<leader>ld', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { buffer = bufnr })
+vim.keymap.set('n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<CR>', { buffer = bufnr })
+
+-- PHP
 require('lspconfig').intelephense.setup({})
 
+-- JavaScript
 require('lspconfig').volar.setup({
   filetypes = { 'vue', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
 })
 
+-- TailwindCSS
 require('lspconfig').tailwindcss.setup({})
 
+-- Go
 require('lspconfig').gopls.setup({
   filetypes = { 'go', 'gomod' },
 })
