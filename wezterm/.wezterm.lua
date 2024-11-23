0b0158a1e54a5abe8bee5647af3b86a51eb37d84
Matus Stafura <kobe21@gmail.com>
2024-11-23 18:38:37 +0100

    wezterm tmux session; fix symlinks

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,25 +7,13 @@ config = {
     font_size = 15.0,
     line_height = 1.3,
     audible_bell = "Disabled",
+    default_prog = { "tmux" },
     window_padding = {
       left = 0,
       right = 0,
       top = 0,
       bottom = 0,
     }
-}
-
-config.colors = {
-    color0 = "#5C5F77",
-    color1 = "#D20F39",
-    color2  = "#40A02B", 
-    color3  = "#DF8E1D", 
-    color4  = "#1E66F5", 
-    color5  = "#EA76CB", 
-    color6  = "#179299", 
-    color7  = "#ACB0BE", 
-    color8 = "#6C6F85", 
-    color9 = "#D20F39", 
 }
 
 return config
