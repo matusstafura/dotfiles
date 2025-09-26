5cff598b021d13522db15fe9f7eee9ee0ddf3463
Matus Stafura <kobe21@gmail.com>
2025-09-26 08:45:21 +0200

    treesitter shortcuts

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -1,10 +1,55 @@
-require('nvim-treesitter.configs').setup({
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+    "php",
+  },
+  textobjects = {
+    select = {
+    enable = true,
+    lookahead = true,
+    keymaps = {
+      -- Functions
+      ["af"] = "@function.outer",
+      ["if"] = "@function.inner",
+
+      -- Classes
+      ["ac"] = "@class.outer",
+      ["ic"] = "@class.inner",
+
+      -- Conditionals
+      ["aC"] = "@conditional.outer",
+      ["iC"] = "@conditional.inner",
+
+      -- Loops
+      ["al"] = "@loop.outer",
+      ["il"] = "@loop.inner",
+
+      -- Blocks
+      ["ab"] = "@block.outer",
+      ["ib"] = "@block.inner",
+
+      -- Parameters
+      ["aa"] = "@parameter.outer",
+      ["ia"] = "@parameter.inner",
+    },
+  },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+      },
+    },
+  },
   highlight = {
     enable = true,
     disable = { 'NvimTree' },
     additional_vim_regex_highlighting = true,
   }
-})
+}
+
 require('ts_context_commentstring').setup({
   enable = true,
   config = {
